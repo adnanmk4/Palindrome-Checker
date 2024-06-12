3ed21f7c458283949d8223acfb959e2735f0bfba
Adnan Muhammad <135832600+adnanmk4@users.noreply.github.com>
2024-06-13 02:42:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const input = document.getElementById('input');
+
+function reverseString(str) {
+    return str.split("").reverse("").join("");
+}
+
+function check (){
+    const value = input.value;
+    const reverse = reverseString(value);
+    
+    if (value === reverse) {
+        alert("P A L I N D R O M E")
+    }
+    else{
+        alert("Not Today!")
+    }
+
+    input.value = "";
+    }
+    
